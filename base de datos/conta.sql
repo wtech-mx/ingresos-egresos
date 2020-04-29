c91d293cd35838955747312fd7667b1df2a663de
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-04-29 16:00:20 -0500

    Update conta.sql
    nuevas tabla y vinculacion

--- base de datos/conta.sql
+++ base de datos/conta.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-04-2020 a las 01:58:37
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+-- Tiempo de generación: 29-04-2020 a las 22:59:44
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -64,8 +65,8 @@ CREATE TABLE `empleado` (
   `telefono` varchar(20) DEFAULT NULL,
   `celular` varchar(20) DEFAULT NULL,
   `registro` varchar(20) DEFAULT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0,
-  `kind` tinyint(1) NOT NULL DEFAULT 0,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `kind` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -75,7 +76,8 @@ CREATE TABLE `empleado` (
 
 INSERT INTO `empleado` (`id`, `dni`, `imagen`, `nombre`, `apellido`, `username`, `email`, `password`, `domicilio`, `localidad`, `telefono`, `celular`, `registro`, `status`, `kind`, `created_at`) VALUES
 (1, '543434', 'view/resources/images/1529887825_user.png', 'tusolutionweb', 'tutos', 'admin', 'admin@admin.com', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', 'AV SAN ANDRES', 'colchester', '9544534', '923344534', '1', 1, 0, '0000-00-00 00:00:00'),
-(2, '456576', 'view/resources/images/default.png', 'Richard', 'Stallman', 'tusolutionweb', 'tusolutionweb@gmail.com', '10470c3b4b1fed12c3baac014be15fac67c6e815', 'av san juan', 'silcon valley', '323445', '932344565', '3', 1, 0, '2018-06-25 03:44:17');
+(2, '456576', 'view/resources/images/default.png', 'Richard', 'Stallman', 'tusolutionweb', 'tusolutionweb@gmail.com', '67a74306b06d0c01624fe0d0249a570f4d093747', 'av san juan', 'silcon valley', '323445', '932344565', '3', 1, 0, '2018-06-25 03:44:17'),
+(3, '543434', 'view/resources/images/default.png', '2020', 'Ramirez', 'Admin', 'dinopiza@yahoo.com.mx', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', 'AV SAN ANDRES', 'CDMX', '+525513730772', '5513730772', 'Administrativo', 1, 0, '2020-04-28 06:15:09');
 
 -- --------------------------------------------------------
 
@@ -105,37 +107,18 @@ INSERT INTO `empleado_permisos` (`idempleado_permiso`, `idempleado`, `idpermiso`
 (9, 1, 9),
 (10, 1, 10),
 (11, 1, 11),
-(12, 2, 1),
-(13, 2, 2),
-(14, 2, 3),
-(15, 2, 4),
-(16, 2, 5),
-(17, 2, 6),
-(18, 2, 7),
-(19, 2, 8),
-(20, 2, 9),
-(21, 2, 10);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empresa`
---
-
-CREATE TABLE `empresa` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `cuit` varchar(30) NOT NULL,
-  `estado` tinyint(4) NOT NULL,
-  `fecha_carga` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `empresa`
---
-
-INSERT INTO `empresa` (`id`, `nombre`, `cuit`, `estado`, `fecha_carga`) VALUES
-(1, 'ibm 2', '133', 1, '2018-06-25 03:45:58');
+(23, 3, 1),
+(24, 2, 1),
+(25, 2, 2),
+(26, 2, 3),
+(27, 2, 4),
+(28, 2, 5),
+(29, 2, 6),
+(30, 2, 7),
+(31, 2, 8),
+(32, 2, 9),
+(33, 2, 10),
+(34, 2, 11);
 
 -- --------------------------------------------------------
 
@@ -145,11 +128,12 @@ INSERT INTO `empresa` (`id`, `nombre`, `cuit`, `estado`, `fecha_carga`) VALUES
 
 CREATE TABLE `gasto` (
   `id` int(11) NOT NULL,
-  `gasto_code` varchar(100) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `gasto_code` int(11) NOT NULL,
   `personal` varchar(50) NOT NULL,
   `concepto` varchar(50) NOT NULL,
   `cantidad` float NOT NULL,
-  `observaciones` varchar(50) NOT NULL DEFAULT '0',
+  `observaciones` varchar(50) NOT NULL,
   `fecha_carga` datetime NOT NULL,
   `foto1` varchar(255) NOT NULL,
   `foto2` varchar(255) NOT NULL,
@@ -162,22 +146,49 @@ CREATE TABLE `gasto` (
 -- Volcado de datos para la tabla `gasto`
 --
 
-INSERT INTO `gasto` (`id`, `gasto_code`, `personal`, `concepto`, `cantidad`, `observaciones`, `fecha_carga`, `foto1`, `foto2`, `foto3`, `foto4`, `foto5`) VALUES
-(1, '1', '2018-06-06', '1', 2, 'faros no funcionan', '2018-06-25 03:48:22', 'view/resources/images/1529891341_asus.jpg', 'view/resources/images/1529891352_cabezas.jpg', 'view/resources/images/1529891346_cerdo2.jpg', 'view/resources/images/1529891357_dejare este momo.jpg', ''),
-(2, '1587772061-1', '', '', 0, '', '2020-04-25 01:47:41', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png'),
-(3, '1587772103-1', '', '', 0, '', '2020-04-25 01:48:23', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png'),
-(4, '1587772163-1', '', '', 0, '', '2020-04-25 01:49:23', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png'),
-(5, '1587772268-1', '', '', 0, '', '2020-04-25 01:51:08', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png', 'view/resources/images/choques.png');
+INSERT INTO `gasto` (`id`, `nombre`, `gasto_code`, `personal`, `concepto`, `cantidad`, `observaciones`, `fecha_carga`, `foto1`, `foto2`, `foto3`, `foto4`, `foto5`) VALUES
+(86, 'PRUEBA', 0, 'pancho', '1500', 5, 'esta bien', '0000-00-00 00:00:00', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `kind`
+-- Estructura de tabla para la tabla `gastos-corriente`
 --
 
-CREATE TABLE `kind` (
+CREATE TABLE `gastos-corriente` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `mes` varchar(50) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `gastos-corriente`
+--
+
+INSERT INTO `gastos-corriente` (`id`, `mes`, `fecha`) VALUES
+(1, 'enero', '2020-04-03 20:00:00'),
+(2, 'febrero', '0000-00-00 00:00:00'),
+(3, 'marzo', '0000-00-00 00:00:00'),
+(4, 'abril', '0000-00-00 00:00:00'),
+(5, 'mayo', '0000-00-00 00:00:00'),
+(6, 'junio', '0000-00-00 00:00:00'),
+(7, 'julio', '0000-00-00 00:00:00'),
+(8, 'agosto', '0000-00-00 00:00:00'),
+(9, 'septiembre', '0000-00-00 00:00:00'),
+(10, 'octubre', '0000-00-00 00:00:00'),
+(11, 'noviembre', '0000-00-00 00:00:00'),
+(12, 'diciembre', '0000-00-00 00:00:00'),
+(13, 'general', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nombre_gasto`
+--
+
+CREATE TABLE `nombre_gasto` (
+  `id` int(11) NOT NULL,
+  `nombre` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -351,12 +362,6 @@ INSERT INTO `vehiculo` (`id`, `vehiculo_code`, `patente`, `marca`, `modelo`, `nr
 --
 
 --
--- Indices de la tabla `configuracion`
---
-ALTER TABLE `configuracion`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `empleado`
 --
 ALTER TABLE `empleado`
@@ -371,21 +376,22 @@ ALTER TABLE `empleado_permisos`
   ADD KEY `idpermiso` (`idpermiso`);
 
 --
--- Indices de la tabla `empresa`
---
-ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `gasto`
 --
 ALTER TABLE `gasto`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gasto_code` (`gasto_code`);
 
 --
--- Indices de la tabla `kind`
+-- Indices de la tabla `gastos-corriente`
 --
-ALTER TABLE `kind`
+ALTER TABLE `gastos-corriente`
+  ADD PRIMARY KEY (`id`,`mes`);
+
+--
+-- Indices de la tabla `nombre_gasto`
+--
+ALTER TABLE `nombre_gasto`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -435,39 +441,33 @@ ALTER TABLE `vehiculo`
 --
 
 --
--- AUTO_INCREMENT de la tabla `configuracion`
---
-ALTER TABLE `configuracion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `empleado_permisos`
 --
 ALTER TABLE `empleado_permisos`
-  MODIFY `idempleado_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT de la tabla `empresa`
---
-ALTER TABLE `empresa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idempleado_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de la tabla `gasto`
 --
 ALTER TABLE `gasto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
--- AUTO_INCREMENT de la tabla `kind`
+-- AUTO_INCREMENT de la tabla `gastos-corriente`
 --
-ALTER TABLE `kind`
+ALTER TABLE `gastos-corriente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `nombre_gasto`
+--
+ALTER TABLE `nombre_gasto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -498,7 +498,7 @@ ALTER TABLE `seguro`
 -- AUTO_INCREMENT de la tabla `taller`
 --
 ALTER TABLE `taller`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `tarjeta`
@@ -522,6 +522,12 @@ ALTER TABLE `vehiculo`
 ALTER TABLE `empleado_permisos`
   ADD CONSTRAINT `empleado_permisos_ibfk_1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`id`),
   ADD CONSTRAINT `empleado_permisos_ibfk_2` FOREIGN KEY (`idpermiso`) REFERENCES `permisos` (`id`);
+
+--
+-- Filtros para la tabla `nombre_gasto`
+--
+ALTER TABLE `nombre_gasto`
+  ADD CONSTRAINT `FK1_nameg_descrg` FOREIGN KEY (`id`) REFERENCES `gasto` (`gasto_code`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
