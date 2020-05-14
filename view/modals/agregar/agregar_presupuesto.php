32e0bc8eab1de9de477f6b3a3fa5ba9d91f8570f
verplancken <42614785+verplancken@users.noreply.github.com>
2020-05-14 18:15:58 -0500

    presupuesto

--- view/modals/agregar/agregar_presupuesto.php
+++ view/modals/agregar/agregar_presupuesto.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header modal-colored-header bg-success">
-                    <h4 class="modal-title" id="myModalLabel">Agregar Nombre Presupuesto Enero</h4>
+                    <h4 class="modal-title" id="myModalLabel">Agregar Nombre Presupuesto</h4>
                     <button type="button" class="close" data-dismiss="modal"
                         aria-hidden="true">×</button>
                 </div>
