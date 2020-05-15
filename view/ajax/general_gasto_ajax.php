b9f1fff2899fe11ebc266fbae076a3dbd890e3c8
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-05-15 01:10:06 -0500

    update genareal

--- view/ajax/general_gasto_ajax.php
+++ view/ajax/general_gasto_ajax.php
@@ -2,37 +2,36 @@
 	include("is_logged.php");//Archivo comprueba si el usuario esta logueado
 	/* Connect To Database*/
 	require_once ("../../config/config.php");
+
 	$query=mysqli_query($con,"SELECT * FROM nombre_gasto WHERE id_mes_nomg=1");
-	 while($row = mysqli_fetch_array($query)){
-            	$nombre=$row['nombre'];
-?>
+
+	 while  ($row = $query->fetch_object()){?>
 
 		<table class="table table-bordered table-striped" id="mytable">
 	        <thead>
 	        	<div id="adicionados"></div>
 	            <tr>
+
 	            	<th>Nombre</th>
 	                <th>Cantidad</th>
 	            </tr>
 	         </thead>
 
 <?php
-
-
-
 			$result = mysqli_query($con,"SELECT fecha, SUM(cantidad) as cantidad_sum FROM gasto group by fecha ");
-            while ($total = $result->fetch_object()){
 
-?>
-
-
+			 foreach ($result as $results): ?>
 	        <tbody>
 	            <tr>
-	            <td><?php echo $nombre; ?></td>
-				<td><?php echo $total->cantidad_sum; ?></td>
+	            <td><?php echo $id = $results['fecha']; ?></td>
+
+				<td><?php echo $cantidad_sum = $results['cantidad_sum']; ?></td>
 	        </tbody>
+			<?php endforeach ?>
 <?php
+
 	}
-}
+
 ?>
 	    </table>
+
