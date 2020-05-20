4e4a7b3f1200a880f57238d1391ead620e182826
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-05-20 11:26:01 -0500

    update function update estado

--- view/ajax/calulos_suma.php
+++ view/ajax/calulos_suma.php
@@ -1,17 +1,71 @@
 <?php
+     include("is_logged.php");//Archivo comprueba si el usuario esta logueado
 
-if (isset($_POST['id'])) {
-    }elseif (isset($_POST['estado'])) {
-    }elseif (
-		!isset($_POST['id'])
-		&& !isset($_POST['estado']))
+	if (empty($_POST['estado'])) {
+        }elseif (empty($_POST['id'])) {
+        }elseif (
+        	!empty($_POST['estado'])
+        	&& !empty($_POST['id'])
+        ){
+			require_once ("../../config/config.php");//Contiene las variables de configuracion para conectar a la base de datos
+
+	        $estado = mysqli_real_escape_string($con,(strip_tags($_POST["estado"],ENT_QUOTES)));
+        	// var_dump($_POST['estado']);
+
+        	$id = $_POST['id'];;
+   //      	var_dump($id);
+
+			// $sql="SELECT id from excedentes_ingresos  where id='".$id."'";
+			// var_dump($sql);
+			//Write register in to database
+			// $sql = "INSERT INTO excedentes_ingresos (estado) VALUES( '".$estado."')" ;
+			$sql = "UPDATE excedentes_ingresos SET estado=$estado WHERE id=$id ";
+				var_dump($sql);
+
+			var_dump($sql);
+			$query_new = mysqli_query($con,$sql);
+            // if has been added successfully
+            if ($query_new) {
+                $messages[] = "Los Datos an sido agregado con éxito.";
+            } else {
+                $errors[] = "Lo sentimos, el registro falló. Por favor, regrese y vuelva a intentarlo.";
+            }
+		}else {
+			$errors[] = "desconocido.";
+		}
+?>
+<script type="text/javascript">
+window.history.go(-1);
+window.history.back();
+</script>
 
 
-include("is_logged.php");//Archivo comprueba si el usuario esta logueado
-require_once ("../../config/config.php");//Contiene las variables de configuracion para conectar a la base de datos
+<?php
+		if (isset($errors)){
+			?>
+			<div class="alert alert-danger" role="alert">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>Error!</strong>
+					<?php
+						foreach ($errors as $error) {
+								echo $error;
+							}
+						?>
+			</div>
+			<?php
+			}
+			if (isset($messages)){
 
-		$id = mysqli_real_escape_string($con,(strip_tags($_POST["id"],ENT_QUOTES)));
-	    $estado = mysqli_real_escape_string($con,(strip_tags($_POST["estado"],ENT_QUOTES)));
-
-
-
+				?>
+				<div class="alert alert-success" role="alert">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<strong>¡Bien hecho!</strong>
+						<?php
+							foreach ($messages as $message) {
+									echo $message;
+								}
+							?>
+				</div>
+				<?php
+			}
+?>
--- view/ajax/ingresos_excedentes_collapese.php
+++ view/ajax/ingresos_excedentes_collapese.php
@@ -50,13 +50,7 @@
                     <option value="2">30%</option>
                 </select>
             </div>
-            <div class="form-group col-md-4">
-             <label class="mr-sm-2" for="estado">Seleccionar Estado</label>
-				<select class="custom-select mr-sm-2" id="estado" name="estado">
-                    <option value="2" selected>No sumar</option>
-                    <option value="1">Sumar</option>
-                </select>
-            </div>
+
 		</div>
 
 		 	<div class="row">
@@ -89,10 +83,10 @@
                 $sw=in_array($excedentes_ingresos->id,$valores);
                 if ($id == $excedentes_ingresos->gasto_code) {
 	            	if ($excedentes_ingresos->servicios ==  1) {
-	            	if($excedentes_ingresos->porcentaje == '1'){
+	            	if($excedentes_ingresos->porcentaje == 1){
 	            		$excedentes_ingresos->porcentaje = '70%';
 	            	}
-	            	if($excedentes_ingresos->porcentaje == '2'){
+	            	if($excedentes_ingresos->porcentaje == 2){
 	            		$excedentes_ingresos->porcentaje = '30%';
 	            	}
 
@@ -111,25 +105,26 @@
                 <td>$<?php echo $excedentes_ingresos->area ?></td>
                 <td>$<?php echo $excedentes_ingresos->monto ?></td>
                 <td><?php echo $excedentes_ingresos->porcentaje ?></td>
-            <?php
-            	if($excedentes_ingresos->porcentaje == '2'){
-            ?>
-	            <td>
+            <td>
+            	<?php
+	            	if($excedentes_ingresos->porcentaje == '30%'){  ?>
+
 	            	<form action="view/ajax/calulos_suma.php" method="post" role="form">
-			            <div class="form-group col-md-4">
+			            <div class="form-group col-md-auto">
+
 			            <label class="mr-sm-2" for="estado">Seleccionar Estado</label>
 							<select class="custom-select mr-sm-2" id="estado" name="estado">
 			                    <option value="2" selected>No sumar</option>
 			                    <option value="1">Sumar</option>
 			                </select>
 			            </div>
-
 	                	<input class="btn btn-success  btn-circle btn-square btn-xs" type="submit" value="<?php echo $excedentes_ingresos->id;?>" name="id">
-
-
 	            	</form>
-	        	</td>
-        	<?php } ?>
+
+	            <?php 	}
+            	 ?>
+
+        	</td>
 
 	        <td class="text-right col-auto">
 
@@ -228,6 +223,7 @@
 	                <th>Monto</th>
 	                <th>Porcentaje</th>
 	                <th>Acciones</th>
+	                <th>Estados</th>
 	            </tr>
 	         </thead>
 			<?php
