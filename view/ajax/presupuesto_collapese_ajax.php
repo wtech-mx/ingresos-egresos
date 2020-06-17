92d7a8c053ff8d8ae1ff1bab724e3ea4f503a38b
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-06-17 09:32:33 -0500

    Update presupuesto_collapese_ajax.php

--- view/ajax/presupuesto_collapese_ajax.php
+++ view/ajax/presupuesto_collapese_ajax.php
@@ -1,3 +1,11 @@
+<?php
+
+$consul = "select * from presupuesto";
+$resul = mysqli_query($con,$consul);
+$num = mysqli_num_rows($resul);
+//si la tabla esta vacia
+if ($num == 0){
+	?>
 <form  role="form" method="post" action="view/ajax/agregar/agregar_presupuesto.php">
 
 		<h1>Adquisición Directa</h1>
@@ -42,6 +50,62 @@
 			    </div>
 		    </div>
 </form>
+<?php
+}else{
+
+	$rspta = mysqli_query($con, "SELECT * FROM presupuesto");
+	$valores=array();
+	//while($row = mysqli_fetch_array($query)){
+	while ($presupuesto = $rspta->fetch_object()){
+	    $sw=in_array($presupuesto->id,$valores);
+	    if ($id == $presupuesto->gasto_code) {
+
+?>
+<form  role="form" method="post" action="view/ajax/agregar/agregar_presupuesto.php">
+
+		<h1>Adquisición Directa</h1>
+
+		    <div class="form-row" style="display: none;">
+			    <div class="form-group col-md-6">
+			      <label for="gasto_code">Id Nombre <?php echo $id ?></label>
+			      <input class="form-control" type="number" value="<?php echo $id ?>" id="gasto_code" name="gasto_code">
+			    </div>
+			</div>
+		    <div class="form-row" style="display: none;">
+			    <div class="form-group col-md-6">
+			      <label for="mes_id">Id MES </label>
+			      <input class="form-control" type="number" value="1" id="mes_id" name="mes_id">
+			    </div>
+			</div>
+			<div class="form-row" style="display: none;">
+			    <div class="form-group col-md-6">
+			      <label for="partida">Partida</label>
+			      <input class="form-control" type="number" value="1" id="partida" name="partida">
+			    </div>
+			</div>
+
+			 <div class="form-row">
+			 	<div class="form-group col-md-6">
+			      <label for="monto">Monto</label>
+			      <input type="text" class="form-control" id="monto" name="monto" value="<?php echo $presupuesto->utilizar?>">
+			    </div>
+			 	<div class="form-group col-md-4">
+			      <label for="utilizado">Utilizado</label>
+			      <input type="number" class="form-control" id="utilizado" name="utilizado">
+			    </div>
+			    <div class="form-group col-md-4" style="display: none;">
+			      <label for="utilizar">Por Utilizar</label>
+		 		  <input type="number" class="form-control" id="utilizar" name="utilizar">
+			    </div>
+		     </div>
+
+		 	<div class="row">
+			 	<div class="col-md-12">
+					<button type="submit" id="guardar_datos_gasto" name="guardar_datos_gasto" class="btn btn-success">Agregar</button>
+			    </div>
+		    </div>
+</form>
+
 		<table class="table table-bordered table-striped" id="mytable">
 	        <thead>
 	        	<div id="adicionados"></div>
@@ -92,7 +156,13 @@
 					}
 			}
 
+    	}else{
+     		 }
+    }
+    }
 ?>
+
+
 	    </table>
 	    <!--===================================================================================================================================================================================================================
  		====================================================================================================RESTRINGIDA====================================================================================================
