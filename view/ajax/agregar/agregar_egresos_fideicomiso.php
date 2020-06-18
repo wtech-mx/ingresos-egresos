0c181aec2949884cb2b7619f081cdebc7cbf22a1
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-06-17 20:21:19 -0500

    update

--- view/ajax/agregar/agregar_egresos_fideicomiso.php
+++ view/ajax/agregar/agregar_egresos_fideicomiso.php
@@ -1,26 +1,26 @@
 <?php
 	include("../is_logged.php");//Archivo comprueba si el usuario esta logueado
 
-		$target_dir="../../resources/images/gastosCorriente/";
-		$image_name = time()."_".basename($_FILES["foto1"]["name"]);
-		$target_file = $target_dir .$image_name ;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		$imageFileZise=$_FILES["foto1"]["size"];
+		// $target_dir="../../resources/images/gastosCorriente/";
+		// $image_name = time()."_".basename($_FILES["foto1"]["name"]);
+		// $target_file = $target_dir .$image_name ;
+		// $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		// $imageFileZise=$_FILES["foto1"]["size"];
 
-		move_uploaded_file($_FILES["foto1"]["tmp_name"], $target_file);
-		$imagen=basename($_FILES["foto1"]["name"]);
-		$img_update="view/resources/images/gastosCorriente/$image_name";
+		// move_uploaded_file($_FILES["foto1"]["tmp_name"], $target_file);
+		// $imagen=basename($_FILES["foto1"]["name"]);
+		// $img_update="view/resources/images/gastosCorriente/$image_name";
 
-		$target_dir2="../../resources/images/gastosCorriente/";
-		$image_name2 = time()."_".basename($_FILES["foto2"]["name"]);
-		$target_file2 = $target_dir2 .$image_name2 ;
-		$imageFileType2 = pathinfo($target_file2,PATHINFO_EXTENSION);
-		$imageFileZise2=$_FILES["foto2"]["size"];
+		// $target_dir2="../../resources/images/gastosCorriente/";
+		// $image_name2 = time()."_".basename($_FILES["foto2"]["name"]);
+		// $target_file2 = $target_dir2 .$image_name2 ;
+		// $imageFileType2 = pathinfo($target_file2,PATHINFO_EXTENSION);
+		// $imageFileZise2=$_FILES["foto2"]["size"];
 
-		/* Fin Validacion*/
-		move_uploaded_file($_FILES["foto2"]["tmp_name"], $target_file2);
-		$imagen2=basename($_FILES["foto2"]["name"]);
-		$img_update2="view/resources/images/gastosCorriente/$image_name2";
+		// /* Fin Validacion*/
+		// move_uploaded_file($_FILES["foto2"]["tmp_name"], $target_file2);
+		// $imagen2=basename($_FILES["foto2"]["name"]);
+		// $img_update2="view/resources/images/gastosCorriente/$image_name2";
 
 	if (empty($_POST['gasto_fide_egresos'])) {
             $errors[] = "gasto_fide_egresos está vacío.";
